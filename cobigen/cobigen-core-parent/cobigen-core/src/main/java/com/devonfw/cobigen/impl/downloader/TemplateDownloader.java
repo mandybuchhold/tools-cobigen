6b3c8b54297cf39a99762ef31f33482f3e5cdd62
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2018-10-19 17:28:59 +0200

    removed ivy again

--- cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/downloader/TemplateDownloader.java
+++ cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/downloader/TemplateDownloader.java
@@ -2,27 +2,15 @@ package com.devonfw.cobigen.impl.downloader;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.ParseException;
-import java.util.Collection;
-import java.util.Optional;
-
-import org.apache.ivy.Ivy;
-import org.apache.ivy.core.module.descriptor.DefaultDependencyDescriptor;
-import org.apache.ivy.core.module.descriptor.DefaultModuleDescriptor;
-import org.apache.ivy.core.module.descriptor.ModuleDescriptor;
-import org.apache.ivy.core.module.id.ModuleRevisionId;
-import org.apache.ivy.core.report.ResolveReport;
-import org.apache.ivy.core.resolve.ResolveOptions;
-import org.apache.ivy.core.retrieve.RetrieveOptions;
-import org.apache.ivy.core.retrieve.RetrieveReport;
-import org.apache.ivy.core.settings.IvySettings;
-import org.apache.ivy.plugins.resolver.IBiblioResolver;
+import java.nio.file.StandardCopyOption;
 
 import com.devonfw.cobigen.api.exception.CobiGenRuntimeException;
-import com.devonfw.cobigen.api.exception.InvalidConfigurationException;
-import com.devonfw.cobigen.impl.config.constant.MavenMetadata;
 
 /**
  * Implementation of {@link TemplateDownloader}.
@@ -42,21 +30,23 @@ public class TemplateDownloader {
      * @return the path of the templates jar
      */
     public Path retrieveTemplates(String groupId, String artifactId, String version) {
-        RetrieveReport report = downloadWithIvy(groupId, artifactId, version);
-
-        Collection<File> copiedFiles = report.getRetrievedFiles();
-        Optional<File> findFirst = copiedFiles.stream()
-            .filter(e -> (!e.getName().matches(".*-javadoc.jar.*") && !e.getName().matches(".*-sources.jar.*")))
-            .findFirst();
-
-        File templatesJar;
-        if (findFirst.isPresent()) {
-            templatesJar = findFirst.get();
-        } else {
-            throw new InvalidConfigurationException("Templates could not be downloaded successfully!");
-        }
-
-        return templatesJar.toPath();
+        // RetrieveReport report = downloadWithIvy(groupId, artifactId, version);
+        //
+        // Collection<File> copiedFiles = report.getRetrievedFiles();
+        // Optional<File> findFirst = copiedFiles.stream()
+        // .filter(e -> (!e.getName().matches(".*-javadoc.jar.*") &&
+        // !e.getName().matches(".*-sources.jar.*")))
+        // .findFirst();
+        //
+        // File templatesJar;
+        // if (findFirst.isPresent()) {
+        // templatesJar = findFirst.get();
+        // } else {
+        // throw new InvalidConfigurationException("Templates could not be downloaded successfully!");
+        // }
+        //
+        // return templatesJar.toPath();
+        return downloadWithIvy(groupId, artifactId, version);
     }
 
     /**
@@ -69,53 +59,84 @@ public class TemplateDownloader {
      *            of the artifact to retrieve
      * @return the {@link RetrieveReport} of the Ivy retrieval
      */
-    private RetrieveReport downloadWithIvy(String groupId, String artifactId, String version) {
-        IvySettings ivySettings = new IvySettings();
-        ivySettings.setDefaultCache(Paths.get(System.getenv("user.home"), ".cobigen", "ivy-cache").toFile());
+    private Path downloadWithIvy(String groupId, String artifactId, String version) {
+        // IvySettings ivySettings = new IvySettings();
+        // ivySettings.setDefaultCache(Paths.get(System.getProperty("user.home"), ".cobigen",
+        // "ivy-cache").toFile());
+        //
+        // IBiblioResolver br = new IBiblioResolver();
+        // br.setM2compatible(true);
+        // br.setUsepoms(true);
+        // br.setName("central");
+        //
+        // ivySettings.addResolver(br);
+        // ivySettings.setDefaultResolver(br.getName());
+        //
+        // Ivy ivy = Ivy.newInstance(ivySettings);
+        //
+        // ResolveOptions ro = new ResolveOptions();
+        // ro.setTransitive(true);
+        // ro.setDownload(true);
+        //
+        // DefaultModuleDescriptor md = DefaultModuleDescriptor
+        // .newDefaultInstance(ModuleRevisionId.newInstance(groupId, artifactId + "-envelope", version));
+        //
+        // ModuleRevisionId ri = ModuleRevisionId.newInstance(groupId, artifactId, version);
+        // DefaultDependencyDescriptor dd = new DefaultDependencyDescriptor(md, ri, false, false, false);
+        //
+        // dd.addDependencyConfiguration("default", "master");
+        // md.addDependency(dd);
+        //
+        // ResolveReport rr;
+        // try {
+        // rr = ivy.resolve(md, ro);
+        // } catch (IOException | ParseException e) {
+        // throw new CobiGenRuntimeException(
+        // "Unable to resolve " + MavenMetadata.GROUP_ID + ":templates-oasp4j:" + version, e);
+        // }
+        // if (rr.hasError()) {
+        // throw new RuntimeException(rr.getAllProblemMessages().toString());
+        // }
+        //
+        // ModuleDescriptor m = rr.getModuleDescriptor();
+        // try {
+        // return ivy.retrieve(m.getModuleRevisionId(),
+        // new RetrieveOptions().setConfs(new String[] { "default" })
+        // .setDestIvyPattern("[organisation]/[module](/[branch])/ivy-[revision].xml").setDestArtifactPattern(
+        // "[organisation]/[module](/[branch])/[type]s/[artifact]-[revision](-[classifier])(.[ext])"));
+        // } catch (IOException e) {
+        // throw new CobiGenRuntimeException(
+        // "Unable to resolve " + MavenMetadata.GROUP_ID + ":templates-oasp4j:" + version, e);
+        // }
 
-        IBiblioResolver br = new IBiblioResolver();
-        br.setM2compatible(true);
-        br.setUsepoms(true);
-        br.setName("central");
-
-        ivySettings.addResolver(br);
-        ivySettings.setDefaultResolver(br.getName());
-
-        Ivy ivy = Ivy.newInstance(ivySettings);
-
-        ResolveOptions ro = new ResolveOptions();
-        ro.setTransitive(true);
-        ro.setDownload(true);
-
-        DefaultModuleDescriptor md = DefaultModuleDescriptor
-            .newDefaultInstance(ModuleRevisionId.newInstance(groupId, artifactId + "-envelope", version));
-
-        ModuleRevisionId ri = ModuleRevisionId.newInstance(groupId, artifactId, version);
-        DefaultDependencyDescriptor dd = new DefaultDependencyDescriptor(md, ri, false, false, false);
-
-        dd.addDependencyConfiguration("default", "master");
-        md.addDependency(dd);
-
-        ResolveReport rr;
-        try {
-            rr = ivy.resolve(md, ro);
-        } catch (IOException | ParseException e) {
-            throw new CobiGenRuntimeException(
-                "Unable to resolve " + MavenMetadata.GROUP_ID + ":templates-oasp4j:" + version, e);
+        String mavenUrl =
+            "https://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=com.devonfw.cobigen&a=templates-oasp4j&v=LATEST";
+        if (false) {
+            mavenUrl = mavenUrl + "&c=sources";
         }
-        if (rr.hasError()) {
-            throw new RuntimeException(rr.getAllProblemMessages().toString());
+        File directory = Paths.get(System.getProperty("user.home"), ".cobigen", "templates-cache").toFile();
+        if (!directory.exists()) {
+            directory.mkdir();
         }
-
-        ModuleDescriptor m = rr.getModuleDescriptor();
+        URL url;
         try {
-            return ivy.retrieve(m.getModuleRevisionId(),
-                new RetrieveOptions().setConfs(new String[] { "default" })
-                    .setDestIvyPattern("[organisation]/[module](/[branch])/ivy-[revision].xml").setDestArtifactPattern(
-                        "[organisation]/[module](/[branch])/[type]s/[artifact]-[revision](-[classifier])(.[ext])"));
+            url = new URL(mavenUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            try (InputStream inputStream = conn.getInputStream()) {
+                String fileName = conn.getURL().getFile().substring(conn.getURL().getFile().lastIndexOf("/") + 1);
+                File file = new File(directory.getPath() + File.separator + fileName);
+                Path targetPath = file.toPath();
+                if (!file.exists()) {
+                    Files.copy(inputStream, targetPath, StandardCopyOption.REPLACE_EXISTING);
+                }
+                return directory.toPath().resolve(fileName);
+            } finally {
+                conn.disconnect();
+            }
         } catch (IOException e) {
-            throw new CobiGenRuntimeException(
-                "Unable to resolve " + MavenMetadata.GROUP_ID + ":templates-oasp4j:" + version, e);
+            throw new CobiGenRuntimeException("Could not retrieve templates artifact.", e);
         }
     }
 
