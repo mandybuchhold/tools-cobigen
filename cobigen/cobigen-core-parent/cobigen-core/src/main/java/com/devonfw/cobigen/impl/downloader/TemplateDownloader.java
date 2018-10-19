8265091b2a279127dad7ea4b72acdd0c2b189238
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2018-10-19 17:35:23 +0200

    short cleanup

--- cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/downloader/TemplateDownloader.java
+++ cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/downloader/TemplateDownloader.java
@@ -1,6 +1,5 @@
 package com.devonfw.cobigen.impl.downloader;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -11,18 +10,24 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import com.devonfw.cobigen.api.exception.CobiGenRuntimeException;
+import com.devonfw.cobigen.impl.config.constant.MavenMetadata;
 
 /**
  * Implementation of {@link TemplateDownloader}.
  */
 public class TemplateDownloader {
 
+    private static final String SOURCE_SUFFIX = "&c=sources";
+
+    private static final String MAVEN_DOWNLOAD_TEMPLATE =
+        "https://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=%s&a=%s&v=%s";
+
     /**
      * Retrieve template jar from maven central.
      * @return the path of the templates jar
      */
     public Path retrieveLatestOaspTemplates() {
-        return retrieveTemplates("com.devonfw.cobigen", "templates-oasp4j", "2.4.3");
+        return retrieveTemplates(MavenMetadata.GROUP_ID, "templates-oasp4j", "LATEST");
     }
 
     /**
@@ -30,23 +35,7 @@ public class TemplateDownloader {
      * @return the path of the templates jar
      */
     public Path retrieveTemplates(String groupId, String artifactId, String version) {
-        // RetrieveReport report = downloadWithIvy(groupId, artifactId, version);
-        //
-        // Collection<File> copiedFiles = report.getRetrievedFiles();
-        // Optional<File> findFirst = copiedFiles.stream()
-        // .filter(e -> (!e.getName().matches(".*-javadoc.jar.*") &&
-        // !e.getName().matches(".*-sources.jar.*")))
-        // .findFirst();
-        //
-        // File templatesJar;
-        // if (findFirst.isPresent()) {
-        // templatesJar = findFirst.get();
-        // } else {
-        // throw new InvalidConfigurationException("Templates could not be downloaded successfully!");
-        // }
-        //
-        // return templatesJar.toPath();
-        return downloadWithIvy(groupId, artifactId, version);
+        return downloadMavenArtifact(groupId, artifactId, version);
     }
 
     /**
@@ -59,64 +48,15 @@ public class TemplateDownloader {
      *            of the artifact to retrieve
      * @return the {@link RetrieveReport} of the Ivy retrieval
      */
-    private Path downloadWithIvy(String groupId, String artifactId, String version) {
-        // IvySettings ivySettings = new IvySettings();
-        // ivySettings.setDefaultCache(Paths.get(System.getProperty("user.home"), ".cobigen",
-        // "ivy-cache").toFile());
-        //
-        // IBiblioResolver br = new IBiblioResolver();
-        // br.setM2compatible(true);
-        // br.setUsepoms(true);
-        // br.setName("central");
-        //
-        // ivySettings.addResolver(br);
-        // ivySettings.setDefaultResolver(br.getName());
-        //
-        // Ivy ivy = Ivy.newInstance(ivySettings);
-        //
-        // ResolveOptions ro = new ResolveOptions();
-        // ro.setTransitive(true);
-        // ro.setDownload(true);
-        //
-        // DefaultModuleDescriptor md = DefaultModuleDescriptor
-        // .newDefaultInstance(ModuleRevisionId.newInstance(groupId, artifactId + "-envelope", version));
-        //
-        // ModuleRevisionId ri = ModuleRevisionId.newInstance(groupId, artifactId, version);
-        // DefaultDependencyDescriptor dd = new DefaultDependencyDescriptor(md, ri, false, false, false);
-        //
-        // dd.addDependencyConfiguration("default", "master");
-        // md.addDependency(dd);
-        //
-        // ResolveReport rr;
-        // try {
-        // rr = ivy.resolve(md, ro);
-        // } catch (IOException | ParseException e) {
-        // throw new CobiGenRuntimeException(
-        // "Unable to resolve " + MavenMetadata.GROUP_ID + ":templates-oasp4j:" + version, e);
-        // }
-        // if (rr.hasError()) {
-        // throw new RuntimeException(rr.getAllProblemMessages().toString());
-        // }
-        //
-        // ModuleDescriptor m = rr.getModuleDescriptor();
-        // try {
-        // return ivy.retrieve(m.getModuleRevisionId(),
-        // new RetrieveOptions().setConfs(new String[] { "default" })
-        // .setDestIvyPattern("[organisation]/[module](/[branch])/ivy-[revision].xml").setDestArtifactPattern(
-        // "[organisation]/[module](/[branch])/[type]s/[artifact]-[revision](-[classifier])(.[ext])"));
-        // } catch (IOException e) {
-        // throw new CobiGenRuntimeException(
-        // "Unable to resolve " + MavenMetadata.GROUP_ID + ":templates-oasp4j:" + version, e);
-        // }
+    private Path downloadMavenArtifact(String groupId, String artifactId, String version) {
 
-        String mavenUrl =
-            "https://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=com.devonfw.cobigen&a=templates-oasp4j&v=LATEST";
-        if (false) {
-            mavenUrl = mavenUrl + "&c=sources";
-        }
-        File directory = Paths.get(System.getProperty("user.home"), ".cobigen", "templates-cache").toFile();
-        if (!directory.exists()) {
-            directory.mkdir();
+        String mavenUrl = String.format(
+            "https://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=%s&a=%s&v=%s",
+            groupId, artifactId, version);
+
+        Path directory = Paths.get(System.getProperty("user.home"), ".cobigen", "templates-cache");
+        if (!Files.exists(directory)) {
+            directory.toFile().mkdirs();
         }
         URL url;
         try {
@@ -126,12 +66,9 @@ public class TemplateDownloader {
             conn.connect();
             try (InputStream inputStream = conn.getInputStream()) {
                 String fileName = conn.getURL().getFile().substring(conn.getURL().getFile().lastIndexOf("/") + 1);
-                File file = new File(directory.getPath() + File.separator + fileName);
-                Path targetPath = file.toPath();
-                if (!file.exists()) {
-                    Files.copy(inputStream, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                }
-                return directory.toPath().resolve(fileName);
+                Path file = directory.resolve(fileName);
+                Files.copy(inputStream, file, StandardCopyOption.REPLACE_EXISTING);
+                return directory.resolve(fileName);
             } finally {
                 conn.disconnect();
             }
